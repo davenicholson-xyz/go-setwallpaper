2e9694bc050160c4c99ecb6ce6f12ccb3748e751
Dave Nicholson <me@davenicholson.xyz>
2025-05-05 22:09:38 +0100

    build specific swtwallpaper

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/davenicholson-xyz/go-setwallpaper
 
 go 1.23.0
+
+require golang.org/x/sys v0.33.0
--- wallpaper/wallpaper.go
+++ wallpaper/wallpaper.go
@@ -4,70 +4,36 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"os/exec"
-	"runtime"
+	"path/filepath"
 	"strings"
 )
 
 var (
-	ErrEmptyFilename      = errors.New("empty filename provided")
-	ErrFileNotExist       = errors.New("file does not exist")
-	ErrUnsupportedDesktop = errors.New("unsupported desktop environment")
-	ErrSetCommandFailed   = errors.New("error setting wallpaper with command")
+	ErrEmptyFilename       = errors.New("empty filename provided")
+	ErrFileNotExist        = errors.New("file does not exist")
+	ErrUnsupportedDesktop  = errors.New("unsupported desktop environment")
+	ErrSetCommandFailed    = errors.New("error setting wallpaper with command")
+	ErrUnsupportedFiletype = errors.New("unspported filetype")
 )
 
-var commands = struct {
-	Linux map[string]string
-	MacOS string
-}{
-	Linux: map[string]string{
-		"gnome": "gsettings set org.gnome.desktop.background picture-uri file://{IMG} && gsettings set org.gnome.desktop.background picture-uri-dark file://{IMG}",
-	},
-	MacOS: "osascript -e 'tell application \"System Events\" to set picture of every desktop to POSIX file \"{IMG}\"'",
-}
-
-func getCommand(desktop string) (string, error) {
-	if runtime.GOOS == "darwin" {
-		return commands.MacOS, nil
-	}
-
-	cmd, exists := commands.Linux["gnome"]
-	if !exists {
-		return "", fmt.Errorf("%w: %s", ErrUnsupportedDesktop, desktop)
-	}
-
-	return cmd, nil
-}
-
-func execCommand(cmd string, filepath string) error {
-
-	cmdString := strings.ReplaceAll(cmd, "{IMG}", filepath)
-
-	output, err := exec.Command("sh", "-c", cmdString).CombinedOutput()
-	if err != nil {
-		fmt.Println(output)
-		return fmt.Errorf("%w: %s", ErrSetCommandFailed)
-	}
-	return nil
-}
-
-func Set(filepath string) error {
-	if filepath == "" {
+func Set(filename string) error {
+	if filename == "" {
 		return ErrEmptyFilename
 	}
 
-	if _, err := os.Stat(filepath); os.IsNotExist(err) {
-		return fmt.Errorf("%w: %s", ErrFileNotExist, filepath)
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return fmt.Errorf("%w: %s", ErrFileNotExist, filename)
 	}
 
-	fmt.Println(filepath)
-
-	cmd, err := getCommand("")
-	if err != nil {
-		return err
+	ext := strings.ToLower(filepath.Ext(filename))
+	switch ext {
+	case ".jpg", ".jpeg", ".png", ".gif", ".bmp":
+		break
+	default:
+		return fmt.Errorf("%w: %s", ErrUnsupportedFiletype, ext)
 	}
 
-	err = execCommand(cmd, filepath)
+	err := setWallpaper(filename)
 	if err != nil {
 		return err
 	}
